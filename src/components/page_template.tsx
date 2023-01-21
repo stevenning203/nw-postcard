1e46e5b8c0e836d694fd28763d8475751e7b5fc3
stevenning.203 <stevenning.203@gmail.com>
2023-01-21 12:55:36 -0800

    more nav and footer

--- src/components/page_template.tsx
+++ src/components/page_template.tsx
@@ -1,15 +1,20 @@
 import Head from "next/head";
+import Footer from "./footer";
 import Navbar from "./navbar";
 
 export default function PageTemplate(props: { children: React.ReactNode, className: string }) {
     return (
-        <div className={props.className}>
+        <div>
             <Head>
                 <title>NWHacks Postcard App</title>
             </Head>
             <Navbar />
-            Hello
-            {props.children}
+            <div className={props.className}>
+                Hello
+                {props.children}
+            </div>
+            <Footer />
         </div>
+
     )
 }
