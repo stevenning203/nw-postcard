d105ff0e5153131ac6465a5307aa264f30049ba5
christinewang766 <christinewang766@gmail.com>
2023-01-21 12:47:56 -0800

    Added striped frame to post card container

--- src/components/postcardElements.tsx
+++ src/components/postcardElements.tsx
@@ -2,6 +2,30 @@ import styled from 'styled-components'
 
 export const PostcardWrap = styled.div`
 background: #ebe3d7;
+display: flex;
+flex-direction: column;
+align-items: center;
+background: #EBE3D7;
+width: 60%;
+height: 80%;
+
+border: 20px solid white;
+border-image: repeating-linear-gradient(
+    45deg,
+    maroon,
+    maroon 20px,
+    #EBE3D7 20px,
+    #EBE3D7 40px
+  )
+  20;
+
+@media (max-width: 1200px) {
+    width: 80%;
+  }
+
+@media (max-width: 600px) {
+    width: 100%;
+  }
 `
 export const PostcardTitle = styled.div`
 
