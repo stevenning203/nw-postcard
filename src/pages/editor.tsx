5e08a3610a1c3179493b8936f9b05eef4eced771
stevenning.203 <stevenning.203@gmail.com>
2023-01-21 17:03:22 -0800

    added submit button

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,12 +1,22 @@
+import Footer from '@/components/footer'
+import { EditorBar } from '@/components/navbar'
+import PageTemplate from '@/components/page_template'
 import PostCard from '@/components/postcard'
+import Head from 'next/head'
 import React from 'react'
 
 const Editor = () => {
-  return (
-    <>
-      <PostCard />
-    </>
-  )
+    return (
+        <div>
+            <Head>
+                <title>Editor</title>
+            </Head>
+            <EditorBar />
+            <PostCard />
+            <Footer />
+        </div>
+
+    )
 }
 
 export default Editor
