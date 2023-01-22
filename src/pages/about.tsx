db434272464062ff60c9dece7a8ab545fb49d8e3
stevenning.203 <stevenning.203@gmail.com>
2023-01-22 10:33:09 -0800

    a

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,13 @@
 import PageTemplate from "@/components/page_template";
+import TinyCard from "@/components/tinyCard";
 
 export default function About() {
     return (
         <PageTemplate>
-            
             <h1 className='text-center text-5xl my-10 underline'>About Post-That</h1>
+            <div className='flex justify-center mt-10'>
+                <TinyCard content="" image="ᯡ࠭䅌ހ匦㢠⸣㕠Ρ盰סᚠ޲Xǐπ໠ঠ⬠ءᲶ䷔崠桠਄䁦㶂⫴yഺҩ৊橠៣傩Ժۭ㍘簔ึᅘ䩬嬥▩䘋䠦ㇷ响㉚俑╺⻝ऺ⾵凔柶䟮敂ㄢቂ⢥⩬ᙖ⛛䉪橎ᕄカ॥ຬა䯗䛴≇愷叒峔時嶑ⓔ媶ⶴ䏗ቐ泳槤㹘䤿ཁ䠀ໂҨᯢ庽㌗Ƶ㽰杼㑵ٗ⤲ಖೇ஻䤡ᾌ䵯⑙磒஁ᔆ殑ᷜ㗘䐃熘紛ഞ䃫ど椴䑍㕿婛᱆繅䥥瘈绋漕ᣞᗍ䠐洡寍ĕ禾ư打᷎狟丒┸ᒺ↶偑䙙惣㗈ື⮗ㄲ㴡႖冩࿘ↂ䗴墢៥ჹ㱭⒇䡹猬ᛔᰡ᮱∯จ缽ആ၄䆉䦑ଁ熖ᝥϡ䆻汑㎾偡⑱ᗉ㯅⯗搖痑檅▤㔬⩱崸娹ᘬ娈娿̀䔘ᆽ廬㯉户䋳瑝⽯凷筣வ浨䔉㷁ᢑ嬶ཌ㹬嵉ⲛ䋦丙㦲庸ᬾ稱屲㯪岸㼌篝婏剒㖓ሴ厅䮈禂冮ⴣഢደᘬ嫝⋭君亓埻牧ż䀥榐C)急㧠+8↣P嗲Ȧ⻠A䀬〣Ⱡᗦ緘´枠ӢӽႶࡳǡ烈㪃厔ᒵጅ瑅⇀穁㌽䪯◌䂋㗙ₐ疸傚㓁㲣倽亥䃔ೕ嘍盔ⓣ攨㝁勇㜌┨悀ѧዌ秖௥㢊ㄸ削坹櫛䎀ѩ‱ᘷ唄⍅䕄斥ㅔࢺ✃⑏ဃ╀䲨㙍໅剉৐ᆣ㘔瑀䤥ゖ㘢㜨䖔㸾䓐ૉͨ≐ᗀ牭ਭ抛む橴㜸ⱬ஬㧕ᗁⓔ咓ැং⨢䷒悿ǘ⑹ಘᒑ㌩䩨䪱䚽໸Ť஼䖫෥㽛೸᫁䥤⼮咥槕಼঍晳㗝Ȓᄵࣀ㽌ᓪ峋˵⢤Ᏽ☷㇒䒛䜔ಝዔ๓ኤ㵜༕曃ఊ䳞๋⛎✪犤∍掷䵕僘່⥪৩䊞⎫ᗉ刐䢈瞹䪬烢取႐䧅公፰倫❩⯙ⓙⳋ凍椊果≺ౙྻᑰ兛ѧ楀悌癢䞟爺঳渐⽿内঴桤㖬ῌ亓ㄧ睢㉪歸᪫Ὃ䫭燗櫣ᝳ೨Ⲕ᱇湷ေ┘橘ᓪ䧇ᄗ梫㽾ᱠ戳ᢴ眳ᦴ波哂䮩矝㯞瑝䞠ـ妚ⶢ寤⺎ⷩ竞矱ʞ憀Ϙ₇涡㞞á㡍挠†຅巐ር⃡Ფ  " from="" to="" sticker="hearts.gif" index={0}></TinyCard>
+            </div>
             <div className='mx-[30%] w-[40%] text-2xl'>
                 <div className="h-5"></div>
                 <p>Post-That allows the user to create and send a personalized postcards.</p>
