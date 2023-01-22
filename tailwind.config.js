b19562753f5fb62e8e8a383a9e4022d32c55c845
stevenning.203 <stevenning.203@gmail.com>
2023-01-22 11:09:52 -0800

    fixed config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}",
-      // Or if using `src` directory:
-      "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
   theme: {
     extend: {
       'animation': {
@@ -24,20 +25,6 @@ module.exports = {
         },
     }
     },
-  },
-  'keyframes': {
-      'text': {
-          '0%, 100%': {
-             'background-size':'200% 200%',
-              'background-position': 'left center'
-          },
-          '50%': {
-             'background-size':'200% 200%',
-              'background-position': 'right center'
-          }
-      },
-  }
-  },
-},
-plugins: [],
+    },
+    plugins: [],
 }
